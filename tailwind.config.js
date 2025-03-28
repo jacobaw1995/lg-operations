1ad36a80d5e0ebf91b92f76db317fed65c35b04d
Jacob Walker <jacobaw1995@gmail.com>
2025-03-27 21:20:24 -0400

    Add Tailwind and PostCSS config files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Scans all files in src
+  ],
+  theme: {
+    extend: {}, // Add customizations here if needed
+  },
+  plugins: [],
+}
